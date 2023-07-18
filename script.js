2e48c5e27b70338e6dfb589834b8dc65cd4bc036
Brianyeel960 <139810633+Brianyeel960@users.noreply.github.com>
2023-07-18 20:29:06 +0000

    insult

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
 // 💻 Create three arrays of strings, one called "randomBodyParts", one called "randomAdjectives", and one called "randomWords".
 // Put at least 10 strings in each
 
-let randomBodyParts = [];
-let randomeAdjectives = [];
-let randomWords = [];
+let randomBodyParts = [
+"head",
+"hand",
+"finger",
+"ears",
+"foot",
+"arm",
+"nose",
+"eyes",
+"eyelids",
 
+];
+
+
+
+
+let randomeAdjectives = [
+"pink",
+"soft",
+"royal",
+"polite",
+"slow",
+];
+
+
+
+let randomWords = [
+
+"otter ",
+"slimy",
+"computer",
+"pear",
+"oranges",
+"dragonfruit",
+"peaches",
+];
 /*
 💻 Write the code to generate the insult here
 */
+let bodyPartIndex = Math.floor(Math.random()*randomBodyParts.length)
+let AdjectiveIdex = Math.floor(Math.random()*randomeAdjectives.length)
+let wordIndex = Math.floor(Math.random()*randomWords.length)
+
+let bodypart = randomBodyParts[bodyPartIndex]
+let adjective = randomeAdjectives[AdjectiveIdex]
+let word = randomWords [wordIndex]
+
+let insult ="your " + bodypart + " is like a "+ adjective + word + "I"
+
+
 
 // Change "Unwritten Insult" to your insult variable
-document.getElementById("insult-spot").innerText = "Unwritten Insult"
+document.getElementById("insult-spot").innerText = insult
 
 
